29305fa2f31f080bde0b8890fbda176b9718e275
Jason E <jasonebueng@gmail.com>
2021-08-16 19:12:56 -0700

    tweaks parent url

--- components/twitch_clip_item_iframe.jsx
+++ components/twitch_clip_item_iframe.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function TwitchClipItemiFrame({ clip, options = {} }) {
     let slug = (typeof clip === 'undefined') ? "" : clip.slug; 
 
-    let src = `https://clips.twitch.tv/embed?clip=${slug}&tt_medium=clips_api&tt_content=embed&autoplay=true&muted=false&parent=jasoneb.github.io/`
+    let src = `https://clips.twitch.tv/embed?clip=${slug}&tt_medium=clips_api&tt_content=embed&autoplay=true&muted=false&parent=jasoneb.github.io`
     let autoplay = options.autoplay || false
 
         return (
